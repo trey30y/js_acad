8027d6ea2db72ba1566399e3ce71f59d6932a76a
Traian <traian30y@yahoo.co.uk>
2026-05-12 00:19:38 +0300

    04 more functions

--- 04-functions/app.js
+++ 04-functions/app.js
@@ -4,13 +4,14 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 
+let gameIsRunning = false;
+
 const getPlayerChoice = function () {
   let cleanedSelection;
   while (true) {
     // If the user clicks __Cancel__ on the prompt dialog, `prompt()` returns `null`.
     const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, "");
     if (selection === null) return "User Cancelled";
-
     const cleanedSelection = selection.toUpperCase().trim();
     if ([ROCK, PAPER, SCISSORS].includes(cleanedSelection)) {
       return cleanedSelection;
@@ -18,7 +19,13 @@ const getPlayerChoice = function () {
     alert("Invalid Choice");
   }
 };
+
 startGameBtn.addEventListener("click", function () {
+  // check if the game is running
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
   console.log("Game is starting...");
   const playerSelection = getPlayerChoice();
   console.log(playerSelection);
