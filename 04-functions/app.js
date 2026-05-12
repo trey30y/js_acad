1bc93582a46a9bfe1dcb3c79953c7759285bf54e
Traian <traian30y@yahoo.co.uk>
2026-05-12 16:41:55 +0300

    bind() 04

--- 04-functions/app.js
+++ 04-functions/app.js
@@ -87,9 +87,64 @@ const sumUp = (resultHandler, ...numbers) => {
   resultHandler(sum);
 };
 
-const showResult = (result) => {
-  console.log(`The result after adding all numbers is: ${result}`);
+const subtractUp = (resultHandler, ...numbers) => {
+  const validateNumber = (number) => (isNaN(number) ? 0 : number);
+  let sum = 0;
+  for (const num of numbers) {
+    sum -= validateNumber(num);
+  }
+  resultHandler(sum);
 };
 
-sumUp(showResult, 100, 4, -7, 77, "P", -6, 8);
-sumUp(showResult, 0, 4, -7, 77, "P", -6, 8);
+const combine = (resultHandler, operation, ...numbers) => {
+  // validateNumber is only available in sumUp
+  const validateNumber = (number) => (isNaN(number) ? 0 : number);
+  let sum = 0;
+  for (const num of numbers) {
+    if (operation === "ADD") {
+      sum += validateNumber(num);
+    } else {
+      sum -= validateNumber(num);
+    }
+  }
+  resultHandler(sum);
+};
+
+const showResult = (messageText, result) => {
+  console.log(`${messageText}: ${result}`);
+};
+
+// bind() will create a new reference at a function which is not immediately executed, which will be preconfigured regarding the arguments it receives
+combine(
+  showResult.bind(this, "The result after adding all numbers is: "),
+  "ADD",
+  100,
+  4,
+  -7,
+  77,
+  "P",
+  -6,
+  8,
+);
+combine(
+  showResult.bind(this, "The result after adding all numbers is: "),
+  "ADD",
+  0,
+  4,
+  -7,
+  77,
+  "P",
+  -6,
+  8,
+);
+combine(
+  showResult.bind(this, "The result after subtracting all numbers is: "),
+  "SUBTRACT",
+  0,
+  4,
+  -7,
+  77,
+  "P",
+  -6,
+  8,
+);
